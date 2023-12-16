75fe99ebf66800852db49a6c05d800b2da471a21
Mathias Flått <mathiasflaatt@gmail.no>
2023-12-16 22:38:58 +0100

    update keymap

--- keypos_def/keypos_adv360.h
+++ keypos_def/keypos_adv360.h
@@ -1,25 +1,17 @@
 /*                                 ADVANTAGE 360 KEY MATRIX / LAYOUT MAPPING
- ╭────────────────────────────┬────────────────────────────╮ ╭─────────────────────────────┬─────────────────────────────╮
- │  0   1   2   3   4   5   6 │  7   8   9  10  11  12  13 │ │ LN5 LN4 LN3 LN2 LN1 LN0 LCN │ RCN RN0 RN1 RN2 RN3 RN4 RN5 │
- │ 14  15  16  17  18  19  20 │ 21  22  23  24  25  26  27 │ │ LT5 LT4 LT3 LT2 LT1 LT0 LCT │ RCT RT0 RT1 RT2 RT3 RT4 RT5 │
- │ 28  29  30  31  32  33  34 │ 39  40  41  42  43  44  45 │ │ LM5 LM4 LM3 LM2 LM1 LM0 LCM │ RCM RM0 RM1 RM2 RM3 RM4 RM5 │
- │ 46  47  48  49  50  51 ╭───┴───╮ 54  55  56  57  58  59 │ │ LB5 LB4 LB3 LB2 LB1 LB0 ╭───┴───╮ RB0 RB1 RB2 RB3 RB4 RB5 │
- │ 60  61  62  63  64╭────╯       ╰────╮71  71  72  73  74 │ │ LG5 LG4 LG3 LG2 LG1 ╭───╯       ╰───╮ RG1 RG2 RG3 RG4 RG5 |
- ╰───────────────────┼────────┬────────┼───────────────────╯ ╰─────────────────────┼───────┬───────┼─────────────────────╯
-                 ╭───╯ 35  36 │ 37  38 ╰───╮                                  ╭────╯LH2 LH3│RH3 RH2╰────╮
-                 │         52 │ 53         │                                  │         LH4│RH4         │
-                 │ 65  65  67 │ 68  69  70 │                                  │ LH1 LH0 LH5│RH5 RH0 RH1 │
-                 ╰────────────┴────────────╯                                  ╰────────────┴────────────╯ */
+ ╭────────────────────────────┬────────────────────────────╮ ╭────────────────────────────┬────────────────────────────╮
+ │  0   1   2   3   4   5   6 │  7   8   9  10  11  12  13 │ │ LN5 LN4 LN3 LN2 LN1 LN0 -- │ --  RN0 RN1 RN2 RN3 RN4 RN5│
+ │ 14  15  16  17  18  19  20 │ 21  22  23  24  25  26  27 │ │ LT5 LT4 LT3 LT2 LT1 LT0 -- │ --  RT0 RT1 RT2 RT3 RT4 RT5│
+ │ 28  29  30  31  32  33  34 │ 35  36  37  38  39  40  41 │ │ LM5 LM4 LM3 LM2 LM1 LM0 -- │ --  RM0 RM1 RM2 RM3 RM4 RM5│
+ │ 42  43  44  45  46  47 ╭───┴───╮ 48  49  50  51  52  53 │ │ LB5 LB4 LB3 LB2 LB1 LB0╭───┴───╮ RB0 RB1 RB2 RB3 RB4 RB5│
+ │ 54  55  56  57  58╭────╯       ╰────╮59  60  61  62  63 │ │ --  --  --  --  --╭────╯       ╰────╮--  --  --  --  -- │
+ ╰───────────────────┼────────┬────────┼───────────────────╯ ╰───────────────────┼────────┬────────┼───────────────────╯
+                 ╭───╯ 64  65 │ 66  67 ╰───╮                                 ╭───╯ --  -- │ --  -- ╰───╮
+                 │ 68  69  70 │ 71  72  73 │                                 │ LH1 LH0 -- │ --  RH0 RH1│
+                 ╰───────╮ 74 │ 75 ╭───────╯                                 ╰───────╮ -- │ -- ╭───────╯
+                         ╰────┴────╯                                                 ╰────┴────╯ */
 
 #pragma once
-
-#define LCN  6  // left-center number
-#define LCT 20  // left-center top
-#define LCM 34  // left-center middle
-
-#define RCN  7  // right-center number
-#define RCT 21  // right-center top
-#define RCM 39  // right-center middle
 
 #define LN0  5  // left-number row
 #define LN1  4
@@ -56,37 +48,31 @@
 #define LM4 29
 #define LM5 28
 
-#define RM0 40  // right-middle row
-#define RM1 41
-#define RM2 42
-#define RM3 43
-#define RM4 44
-#define RM5 45
+#define RM0 36  // right-middle row
+#define RM1 37
+#define RM2 38
+#define RM3 39
+#define RM4 40
+#define RM5 41
 
-#define LB0 51  // left-bottom row
-#define LB1 50
-#define LB2 49
-#define LB3 48
-#define LB4 47
-#define LB5 46
+#define LB0 47  // left-bottom row
+#define LB1 46
+#define LB2 45
+#define LB3 44
+#define LB4 43
+#define LB5 42
 
-#define RB0 54  // right-bottom row
-#define RB1 55
-#define RB2 56
-#define RB3 57
-#define RB4 58
-#define RB5 59
+#define RB0 48  // right-bottom row
+#define RB1 49
+#define RB2 50
+#define RB3 51
+#define RB4 52
+#define RB5 53
 
-#define LH0 66  // left thumb keys
-#define LH1 65
-#define LH2 35
-#define LH3 36
-#define LH4 52
-#define LH5 67
+#define LH0 70  // left thumb keys
+#define LH1 69
+#define LH2 74
 
-#define RH0 69  // right thumb keys
-#define RH1 70
-#define RH2 38
-#define RH3 37
-#define RH4 53
-#define RH5 68
+#define RH0 72  // right thumb keys
+#define RH1 73
+#define RH2 75
